38f8da2586344fee14ff1abb30765eb1b8b82a42
Klaudia Drożdż <klaudiadrozdz420@gmail.com>
2020-03-17 16:47:40 +0100

    Add new files

---
+++ 2B-Lab-2/DB.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2B_Lab_2
+{
+    public class DB
+    {
+        //lista klientów
+        public void Select(SqlConnection connection)
+        {
+            var query = "SELECT * FROM Customers"; 
+            var command = new SqlCommand(query, connection); 
+
+            var reader = command.ExecuteReader(); 
+            reader.Read(); 
+
+            while (reader.Read())
+            {
+                
+                Console.WriteLine(reader["CompanyName"]);
+            }
+            reader.Close(); 
+        }
+
+        //wstawianie danych do tabeli Region
+        public void Insert(SqlConnection connection, int id, string description)
+        {
+            var query = "INSERT INTO Region(RegionID, RegionDescription)" +
+                "VALUES (@id, @description)";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@description", description);
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} row affected");
+        }
+
+        //usuwanie danych z taneli Region
+        public void Delete(SqlConnection connection, int id)
+        {
+            var query = "DELETE FROM Region WHERE RegionID = @id";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+
+            var deleted = command.ExecuteNonQuery();
+            Console.WriteLine($"{deleted} row deleted");
+
+        }
+
+        //aktualizacja danych w tabeli Region
+        public void Update(SqlConnection connection, int id, string description)
+        {
+            var query = "UPDATE Region SET RegionDescription = @description" + "WHERE RegionID = @id";
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@description", description);
+
+            var update = command.ExecuteNonQuery();
+            Console.WriteLine($"{update} row updated");
+
+        }
+    }
+}
---
+++ 2B-Lab-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2B_Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                connection.Open();
+                var db = new DB();
+                db.Select(connection);
+                db.Delete(connection, 11);
+                db.Insert(connection, 11, "Bielsko");
+                db.Insert(connection, 202, "d-test");
+                db.Delete(connection, 201);
+                connection.Close();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
